5b713434ebd5ca2114d51a9e0e2fff7d9a072a8e
nlantz <nicholaslantz@gmail.com>
2014-12-21 11:06:52 -0700

    project setup

---
+++ Swarm_Rover_V1_0/Com_Handeler.ino
@@ -0,0 +1,6 @@
+// Author = Nicholas Lantz
+// Email = Nicholaslantz@gmail.com
+// Date = 21Dec2014
+
+//This code is a communications handeler for the Swarm Vehecles 
+//as part of the Autonomous Swarm Transport System
---
+++ Swarm_Rover_V1_0/Swarm_Rover_V1_0.ino
@@ -0,0 +1,6 @@
+// Author = Nicholas Lantz
+// Email = Nicholaslantz@gmail.com
+// Date = 21Dec2014
+
+//This code is the entry point for the Swarm Vehicle software
+//as part of the Autonomous Swarm Transport System
